b06a25bd2e50c2d79b2421715c1eda0530a38e1e
bosedipayan <bosedipayan2000@gmail.com>
2024-07-29 18:15:09 +0530

    Modified footer part

--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/bosedipayan"
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://x.com/BoseJisu01"
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/dipayan-bose-658b50186/"
   },
 ];
