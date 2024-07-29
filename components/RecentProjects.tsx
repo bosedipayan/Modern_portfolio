48d2fdde77590ffc5c727b89d8ce22b05c01d38f
bosedipayan <bosedipayan2000@gmail.com>
2024-07-29 14:33:37 +0530

    Added sentry for reporting bugs and stated with clients

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,7 +7,7 @@ import { PinContainer } from "./ui/Pin";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
@@ -65,12 +65,14 @@ const RecentProjects = () => {
                   ))}
                 </div>
 
-                <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
-                  </p>
-                  <FaLocationArrow className="ms-3" color="#CBACF9" />
-                </div>
+                <a href={item.link} target="_blank">
+                  <div className="flex justify-center items-center">
+                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                      Check Live Site
+                    </p>
+                    <FaLocationArrow className="ms-3" color="#CBACF9" />
+                  </div>
+                </a>
               </div>
             </PinContainer>
           </div>
