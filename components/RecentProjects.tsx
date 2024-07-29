24fb7360c78969434cfcaac82d3f50624ac0d383
bosedipayan <bosedipayan2000@gmail.com>
2024-07-29 17:26:29 +0530

    Modified recent projects and added testimonials section

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,7 +10,7 @@ const RecentProjects = () => {
     <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
-        <span className="text-purple">recent projects</span>
+        <span className="text-sky-300">recent projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
